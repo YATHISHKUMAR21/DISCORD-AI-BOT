d31838828ab9be3a2308a4a8a023cf114d721da9
YATHISHKUMAR21 <appuaishu5@gmail.com>
2026-03-29 13:03:20 +0530

    discord bot reply

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const {Client, GatewayIntentBits} = require("discord.js");
+require("dotenv").config();
 
 const client = new Client({
     intents: [
@@ -12,4 +13,22 @@ client.once("ready", () => {
     console.log("Bot is ready!");
 });
 
-client.login("YOUR_BOT_TOKEN_HERE");
+client.on("messageCreate", (message) => {
+    // console.log(`Received message: ${message.content}`);
+    // console.log(message.member.user);
+    const attachements = message
+    const isBot = message.author.bot;
+    if (isBot) {
+        return;
+    }
+
+    message.reply(`Hello ${message.author.username}! I am a yathish bot.`);
+
+
+    // message.reply("Hello! I am a yathish bot.");
+    // if (message.content === "ping") {
+    //     message.reply("pong");
+    // }
+});
+
+client.login(process.env.DISCORD_BOT_TOKEN);
